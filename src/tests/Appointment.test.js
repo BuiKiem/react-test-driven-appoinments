a1270a5e88734c8986eca4168d1ad33b026cf287
Bui Kiem <buivantrung1105@gmail.com>
2020-02-10 18:36:20 +0700

    'AppointmentsDayView' should render a list of appointments

--- src/tests/Appointment.test.js
+++ src/tests/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-import {Appointment} from "../components/Appoinment";
+import { Appointment, AppointmentsDayView } from "../components/Appoinment";
 
 describe('Appointment', function () {
   let container;
@@ -28,5 +28,48 @@ describe('Appointment', function () {
     render(<Appointment customer={customer} />);
 
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  const render = component => ReactDOM.render(component, container);
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  it('should render a div with the right id', function () {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+
+  it('should render multiple appointments in an ol element', function () {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
+
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelector("ol")).not.toBeNull();
+    expect(container.querySelector("ol").children).toHaveLength(2);
+  });
+
+  it('should render each appointment in an li', function () {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
+
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
+    expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
   });
 });
