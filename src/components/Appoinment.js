700197a84636c5bd7148ce18fd51fe97f2547861
Bui Kiem <buivantrung1105@gmail.com>
2020-02-11 15:20:29 +0700

    AppoinmentsDayView should render another appointment when selected.

--- src/components/Appoinment.js
+++ src/components/Appoinment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const appointmentTimeOfDay = startsAt => {
   const [h, m] = new Date(startsAt).toTimeString().split(":");
@@ -10,11 +10,13 @@ export const Appointment = ({ customer }) => {
 };
 
 export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
   return <div id="appointmentsDayView">
     <ol>
-      {appointments.map(appointment => (
+      {appointments.map((appointment, index) => (
         <li key={appointment.startsAt}>
-          <button type="button">
+          <button type="button" onClick={() => setSelectedAppointment(index)}>
             {appointmentTimeOfDay(appointment.startsAt)}
           </button>
         </li>))}
@@ -23,7 +25,7 @@ export const AppointmentsDayView = ({ appointments }) => {
       appointments.length === 0 ? (
         <p>There are no appointments scheduled for today.</p>
       ) : (
-        <Appointment {...appointments[0]} />
+        <Appointment {...appointments[selectedAppointment]} />
       )
     }
   </div>;
