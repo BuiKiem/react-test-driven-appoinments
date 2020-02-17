fb973e1c0d6add9a251db23638ecb03a880a1ab7
Bui Kiem <buivantrung1105@gmail.com>
2020-02-17 15:52:11 +0700

    'AppointmentForm' service field should render as a select box.

--- src/tests/AppointmentForm.test.js
+++ src/tests/AppointmentForm.test.js
@@ -6,14 +6,22 @@ import { AppointmentForm } from "../components/AppointmentForm";
 describe("AppointmentForm", function() {
   let render, container;
 
+  const form = id => container.querySelector(`form[id="${id}"]`);
+
   beforeEach(() => {
     ({ render, container } = createContainer());
   });
 
-  const form = id => container.querySelector(`form[id="${id}"]`);
-
   it("should render a form", function() {
     render(<AppointmentForm />);
     expect(form("appointment")).not.toBeNull();
+  });
+
+  describe("service field", function() {
+    it("should render as a select box", function() {
+      render(<AppointmentForm />);
+      expect(form("appointment").elements.service).not.toBeNull();
+      expect(form("appointment").elements.service.tagName).toEqual("SELECT");
+    });
   });
 });
