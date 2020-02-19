cf8ea7aadcc9ff2aa9b2d776a87913070084b1da
Bui Kiem <buivantrung1105@gmail.com>
2020-02-19 18:12:29 +0700

    Manually testing 'AppointmentForm'

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import "./index.css";
 import { AppointmentsDayView } from "./components/AppointmentsDayView";
 
 import { sampleAppointments } from "./sampleData";
+import { AppointmentForm } from "./components/AppointmentForm";
 
 ReactDOM.render(
-  <AppointmentsDayView appointments={sampleAppointments} />,
+  <div>
+    <AppointmentsDayView appointments={sampleAppointments} />
+    <AppointmentForm availableTimeSlots={sampleAppointments} />
+  </div>,
   document.getElementById("root"),
 );
